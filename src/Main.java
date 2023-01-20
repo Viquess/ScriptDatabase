43a890a486f43c92270903c8a9c835f2d5e0067f
Viques <vincenzo.bova05@gmail.com>
2023-01-20 14:22:13 +0100

    Modificato il programma usato i catch al posto del throws Exception

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.Random;
 import java.util.UUID;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
         var startTime = System.currentTimeMillis();
 
         var names = Lists.newArrayList("Mattia", "Marco", "Luca", "Giovanni", "Matteo", "Francesco", "Andrea", "Vincenzo", "Giuseppe", "Loris", "Mario",
@@ -24,108 +24,114 @@ public class Main {
                 "Isteroscopia", "Cistoscopia", "Astroscopia", "Laparoscopia", "Mediastinoscopia", "Toracoscopia", "Biopsia");
 
         var driverName = "com.mysql.cj.jdbc.Driver";
-        Class.forName(driverName);
+        try {
+            Class.forName(driverName);
 
-        System.out.println("Inizializzo la connessione...");
-        var connection =  DriverManager.getConnection("jdbc:mysql://localhost", "root", null);
+            System.out.println("Inizializzo la connessione...");
+            var connection = DriverManager.getConnection("jdbc:mysql://localhost", "root", null);
 
-        var statement = connection.createStatement();
-        statement.executeUpdate("DROP DATABASE IF EXISTS hospital");
-        statement.executeUpdate("CREATE DATABASE hospital");
-        System.out.println("Database creato con successo!");
+            var statement = connection.createStatement();
+            statement.executeUpdate("DROP DATABASE IF EXISTS hospital");
+            statement.executeUpdate("CREATE DATABASE hospital");
+            System.out.println("Database creato con successo!");
 
-        connection =  DriverManager.getConnection("jdbc:mysql://localhost/hospital", "root", null);
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/hospital", "root", null);
 
-        statement = connection.createStatement();
-        statement.executeUpdate("CREATE TABLE reparti (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)");
-        System.out.println("Creata la tabella reparti!");
+            statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE reparti (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)");
+            System.out.println("Creata la tabella reparti!");
 
-        statement.executeUpdate("CREATE TABLE pazienti (cf VARCHAR(16) PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, sesso ENUM('m', 'f') NOT NULL)");
-        System.out.println("Creata la tabella pazienti!");
+            statement.executeUpdate("CREATE TABLE pazienti (cf VARCHAR(16) PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, sesso ENUM('m', 'f') NOT NULL)");
+            System.out.println("Creata la tabella pazienti!");
 
-        statement.executeUpdate("CREATE TABLE medici (cf VARCHAR(16) PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, date VARCHAR(10) NOT NULL, FK_idRep INT NOT NULL, FOREIGN KEY (FK_idRep) REFERENCES reparti(id))");
-        System.out.println("Creata la tabella medici!");
+            statement.executeUpdate("CREATE TABLE medici (cf VARCHAR(16) PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, date VARCHAR(10) NOT NULL, FK_idRep INT NOT NULL, FOREIGN KEY (FK_idRep) REFERENCES reparti(id))");
+            System.out.println("Creata la tabella medici!");
 
-        statement.executeUpdate("CREATE TABLE visite (id INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(10) NOT NULL, esito VARCHAR(255) NOT NULL, FK_cfPaz VARCHAR(16) NOT NULL, FK_cfMed VARCHAR(16) NOT NULL, " +
-                "FOREIGN KEY (FK_cfPaz) REFERENCES pazienti(cf), FOREIGN KEY (FK_cfMed) REFERENCES medici(cf))");
-        System.out.println("Creata la tabella visite!");
+            statement.executeUpdate("CREATE TABLE visite (id INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(10) NOT NULL, esito VARCHAR(255) NOT NULL, FK_cfPaz VARCHAR(16) NOT NULL, FK_cfMed VARCHAR(16) NOT NULL, " +
+                    "FOREIGN KEY (FK_cfPaz) REFERENCES pazienti(cf), FOREIGN KEY (FK_cfMed) REFERENCES medici(cf))");
+            System.out.println("Creata la tabella visite!");
 
-        statement.executeUpdate("CREATE TABLE ricoveri (id INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(10) NOT NULL, durata INT NOT NULL, FK_idRep INT NOT NULL, FK_cfPaz VARCHAR(16) NOT NULL, " +
-                "FOREIGN KEY (FK_cfPaz) REFERENCES pazienti(cf), FOREIGN KEY (FK_idRep) REFERENCES reparti(id))");
-        System.out.println("Creata la tabella ricoveri!");
+            statement.executeUpdate("CREATE TABLE ricoveri (id INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(10) NOT NULL, durata INT NOT NULL, FK_idRep INT NOT NULL, FK_cfPaz VARCHAR(16) NOT NULL, " +
+                    "FOREIGN KEY (FK_cfPaz) REFERENCES pazienti(cf), FOREIGN KEY (FK_idRep) REFERENCES reparti(id))");
+            System.out.println("Creata la tabella ricoveri!");
 
-        statement.executeUpdate("CREATE TABLE esami (id INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(10) NOT NULL, tipo VARCHAR(255) NOT NULL, esito VARCHAR(255) NOT NULL, FK_cfPaz VARCHAR(16) NOT NULL," +
-                "FOREIGN KEY (FK_cfPaz) REFERENCES pazienti(cf))");
-        System.out.println("Creata la tabella esami!");
+            statement.executeUpdate("CREATE TABLE esami (id INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(10) NOT NULL, tipo VARCHAR(255) NOT NULL, esito VARCHAR(255) NOT NULL, FK_cfPaz VARCHAR(16) NOT NULL," +
+                    "FOREIGN KEY (FK_cfPaz) REFERENCES pazienti(cf))");
+            System.out.println("Creata la tabella esami!");
 
-        for (String reparto : reparti)
-            statement.executeUpdate("INSERT INTO reparti (id, name) VALUES (null, '%s')".formatted(reparto));
+            for (String reparto : reparti)
+                statement.executeUpdate("INSERT INTO reparti (id, name) VALUES (null, '%s')".formatted(reparto));
 
-        System.out.println("\nTabella reparti popolata con successo!");
+            System.out.println("\nTabella reparti popolata con successo!");
 
-        for (int i = 0; i < 100; i++) {
-            var num = randOf(44);
-            var name = names.get(num);
-            var surname = surnames.get(randOf(24));
-            var sex = num <= 20 ? 'm' : 'f';
+            for (int i = 0; i < 100; i++) {
+                var num = randOf(44);
+                var name = names.get(num);
+                var surname = surnames.get(randOf(24));
+                var sex = num <= 20 ? 'm' : 'f';
 
-            statement.executeUpdate("INSERT INTO pazienti (cf, name, surname, sesso) VALUES ('%s', '%s', '%s', '%s')".formatted(randCf(), name, surname, sex));
+                statement.executeUpdate("INSERT INTO pazienti (cf, name, surname, sesso) VALUES ('%s', '%s', '%s', '%s')".formatted(randCf(), name, surname, sex));
+            }
+            System.out.println("Tabella pazienti popolata con successo!");
+
+            for (int i = 0; i < 25; i++) {
+                var name = names.get(randOf(44));
+                var surname = surnames.get(randOf(24));
+
+                statement.executeUpdate("INSERT INTO medici (cf, name, surname, date, FK_idRep) VALUES ('%s', '%s', '%s', '%s', '%s')".formatted(randCf(), name, surname, randDate(1965, 2000), (randOf(6) + 1)));
+            }
+            System.out.println("Tabella medici popolata con successo!");
+
+            for (int i = 0; i < 400; i++) {
+                var esito = randOf(2) == 1 ? "POSITIVO" : "NEGATIVO";
+                var res = statement.executeQuery("SELECT cf FROM pazienti ORDER BY RAND() LIMIT 1");
+                String cfPaz = null;
+
+                while (res.next())
+                    cfPaz = res.getString("cf");
+
+                res = statement.executeQuery("SELECT cf FROM medici ORDER BY RAND() LIMIT 1");
+                String cfMed = null;
+
+                while (res.next())
+                    cfMed = res.getString("cf");
+
+                statement.executeUpdate("INSERT INTO visite (id, date, esito, FK_cfPAZ, FK_cfMED) VALUES (null, '%s', '%s', '%s', '%s')".formatted(randDate(2015, 2022), esito, cfPaz, cfMed));
+            }
+            System.out.println("Tabella visite popolata con successo!");
+
+            for (int i = 0; i < 150; i++) {
+                var res = statement.executeQuery("SELECT cf FROM pazienti ORDER BY RAND() LIMIT 1");
+                String cfPaz = null;
+
+                while (res.next())
+                    cfPaz = res.getString("cf");
+
+                statement.executeUpdate("INSERT INTO ricoveri (id, date, durata, FK_idREP, FK_cfPAZ) VALUES (null, '%s', '%s', '%s', '%s')".formatted(randDate(2015, 2022), randOf(30), randOf(6) + 1, cfPaz));
+            }
+            System.out.println("Tabella ricoveri popolata con successo!");
+
+            for (int i = 0; i < 600; i++) {
+                var esito = randOf(2) == 1 ? "POSITIVO" : "NEGATIVO";
+                var res = statement.executeQuery("SELECT cf FROM pazienti ORDER BY RAND() LIMIT 1");
+                String cfPaz = null;
+
+                while (res.next())
+                    cfPaz = res.getString("cf");
+
+                statement.executeUpdate("INSERT INTO esami (id, date, tipo, esito, FK_cfPAZ) VALUES (null, '%s', '%s', '%s', '%s')".formatted(randDate(2015, 2022), esami.get(randOf(14)), esito, cfPaz));
+            }
+            System.out.println("Tabella esami popolata con successo!");
+
+            statement.close();
+
+            var time = System.currentTimeMillis() - startTime;
+            System.out.printf("\nEsecuzione terminata in %s secondi (%sms).", (time / 1000), time);
+        } catch (SQLException e) {
+            System.out.println("Errore di connessione al database!");;
+        } catch (ClassNotFoundException e) {
+            System.out.println("Errore nel definire la classe com.mysql.cj.jdbc.Driver.");;
         }
-        System.out.println("Tabella pazienti popolata con successo!");
-
-        for (int i = 0; i < 25; i++) {
-            var name = names.get(randOf(44));
-            var surname = surnames.get(randOf(24));
-
-            statement.executeUpdate("INSERT INTO medici (cf, name, surname, date, FK_idRep) VALUES ('%s', '%s', '%s', '%s', '%s')".formatted(randCf(), name, surname, randDate(1965, 2000), (randOf(6) + 1)));
-        }
-        System.out.println("Tabella medici popolata con successo!");
-
-        for (int i = 0; i < 400; i++) {
-            var esito = randOf(2) == 1 ? "POSITIVO" : "NEGATIVO";
-            var res = statement.executeQuery("SELECT cf FROM pazienti ORDER BY RAND() LIMIT 1");
-            String cfPaz = null;
-
-            while (res.next())
-                cfPaz = res.getString("cf");
-
-            res = statement.executeQuery("SELECT cf FROM medici ORDER BY RAND() LIMIT 1");
-            String cfMed = null;
-
-            while (res.next())
-                cfMed = res.getString("cf");
-
-            statement.executeUpdate("INSERT INTO visite (id, date, esito, FK_cfPAZ, FK_cfMED) VALUES (null, '%s', '%s', '%s', '%s')".formatted(randDate(2015, 2022), esito, cfPaz, cfMed));
-        }
-        System.out.println("Tabella visite popolata con successo!");
-
-        for (int i = 0; i < 150; i++) {
-            var res = statement.executeQuery("SELECT cf FROM pazienti ORDER BY RAND() LIMIT 1");
-            String cfPaz = null;
-
-            while (res.next())
-                cfPaz = res.getString("cf");
-
-            statement.executeUpdate("INSERT INTO ricoveri (id, date, durata, FK_idREP, FK_cfPAZ) VALUES (null, '%s', '%s', '%s', '%s')".formatted(randDate(2015, 2022), randOf(30), randOf(6) + 1, cfPaz));
-        }
-        System.out.println("Tabella ricoveri popolata con successo!");
-
-        for (int i = 0; i < 600; i++) {
-            var esito = randOf(2) == 1 ? "POSITIVO" : "NEGATIVO";
-            var res = statement.executeQuery("SELECT cf FROM pazienti ORDER BY RAND() LIMIT 1");
-            String cfPaz = null;
-
-            while (res.next())
-                cfPaz = res.getString("cf");
-
-            statement.executeUpdate("INSERT INTO esami (id, date, tipo, esito, FK_cfPAZ) VALUES (null, '%s', '%s', '%s', '%s')".formatted(randDate(2015, 2022), esami.get(randOf(14)) , esito, cfPaz));
-        }
-        System.out.println("Tabella esami popolata con successo!");
-
-        statement.close();
-
-        var time = System.currentTimeMillis() - startTime;
-        System.out.printf("\nEsecuzione terminata in %s secondi (%sms).", (time/1000), time);
     }
 
     private static int randOf(int max) {
